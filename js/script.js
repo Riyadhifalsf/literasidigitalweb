cdf945c2cb200386e34d3cf1b9fc0a7324657a44
Babeh <babeh@Kali.babeh.com>
2024-12-10 23:10:47 -0500

    deleted:    css/bootstrap-theme.css.map 	deleted:    css/bootstrap-theme.min.css.map 	deleted:    css/bootstrap.css.map 	deleted:    css/bootstrap.min.css.map 	modified:   css/style.css 	deleted:    img/Profile1.png 	new file:   img/digitaltech.jpg 	deleted:    img/header-bg.JPG 	new file:   img/jumbotron.jpg 	deleted:    img/thumbs/1.jpg 	deleted:    img/thumbs/2.jpg 	deleted:    img/thumbs/3.jpg 	deleted:    img/thumbs/4.jpg 	deleted:    img/thumbs/5.jpg 	deleted:    img/thumbs/6.jpg 	modified:   index.html 	modified:   js/script.js 	new file:   pages/test.html 	new file:   pages/work.html

--- js/script.js
+++ js/script.js
@@ -1,54 +1,111 @@
-// Event pada saat link di klik
-$('.page-scroll').on('click', function(e){
+    $('.page-scroll').on('click', function(e) {
+        e.preventDefault();  // Mencegah perilaku default dari link
 
-    // Ambil isi href
-    var tujuan = $(this).attr('href');
-    // tangkap elemen yang bersangkutan
-    var elemenTujuan = $(tujuan);
+        // Mengambil target URL dari atribut href
+        let tujuan = $(this).attr('href');
+        let elemenTujuan = $(tujuan);
 
-    // Pindahkan scroll
-    $('html, body').animate({
-        scrollTop: elemenTujuan.offset().top -50
-    }/*, Kecepatan Scrolling*/ /*1000*/ /*Efek*/ /*, 'swing'*/ /*'linear'*/, 'easeInOutExpo');
+        $('a[href="#home"]').addClass('active');
 
-    e.pereventDefault();
+        // Menghapus kelas 'active' dari semua elemen dengan kelas 'page-scroll'
+        $('.page-scroll').removeClass('active');
 
-});
+        // Menambahkan kelas 'active' pada elemen yang diklik
+        $(this).addClass('active');
 
-// Parallax
-$(window).scroll(function() {
-    var wScroll = $(this).scrollTop();
-
-    // Jumbotron
-    $('.jumbotron img').css({
-        'transform' : 'translate(0px, '+ wScroll/2 +'%)'
-    });
-
-    $('.jumbotron h1').css({
-        'transform' : 'translate(0px, '+ wScroll/1 +'%)'
-    });
-
-    $('.jumbotron p').css({
-        'transform' : 'translate(0px, '+ wScroll/.5  +'%)'
+        // Animasi scroll ke elemen tujuan dengan offset 80px
+        $('html, body').animate({
+            scrollTop: elemenTujuan.offset().top - 200
+        }, 1000, 'easeInOutExpo');
     });
 
 
-    // Portfolio
-    if( wScroll > $('.portfolio').offset().top - 250 ) {
-        $('.portfolio .thumbnail').each(function(i) {
-            setTimeout(function() {
-                $('.portfolio .thumbnail').eq(i).addClass('muncul');
-            }, 300 * (i+1));
+    // Bersihkan url
+    $(document).ready(function() {
+        // Menangani klik tautan pada halaman 'Work' (atau halaman lainnya)
+        $('.page-scroll').on('click', function(e) {
+            e.preventDefault();  // Mencegah perilaku default dari link
+    
+            let tujuan = $(this).attr('href'); // Mengambil href (target link)
+            let elemenTujuan;
+            
+            if (tujuan.includes('#')) {
+                // Jika tujuan mengandung '#', ambil elemen berdasarkan ID
+                elemenTujuan =  $(tujuan) ; 
+            } else {
+                // Jika bukan hash, pastikan kita scroll ke bagian yang relevan
+                return;
+            }
+    
+            // Hapus hash dari URL tanpa reload halaman
+            if (window.history.pushState) {
+                window.history.pushState("", document.title, window.location.pathname + window.location.search);
+            } else {
+                window.location.hash = "";
+            }
         });
-        
+    });
+
+
+// Fungsi untuk memeriksa apakah media query cocok
+function checkMediaQuery() {
+    const wScroll = $(window).scrollTop();
+    const mediaQuery = window.matchMedia('(min-width: 768px)'); // Untuk desktop atau lebih besar
+
+    if (mediaQuery.matches) {
+        // Jika ukuran layar >= 768px (desktop), aktifkan parallax
+        applyParallax(wScroll);
+    } else {
+        // Jika ukuran layar < 768px (mobile), nonaktifkan parallax
+        // Pada perangkat mobile, kita tidak menerapkan efek parallax
+        resetParallax();
     }
+}
 
+// Fungsi untuk menerapkan efek parallax
+function applyParallax(wScroll) {
+    // Jumbotron
+    $('.jumbotron-main h1').css({
+        'transform' : 'translate(0px, '+ wScroll/.9 +'%)'
+    });
 
+    $('.jumbotron-main p').css({
+        'transform' : 'translate(0px, '+ wScroll/.74 +'%)'
+    });
 
+    $('.jumbotron-main button').css({
+        'transform' : 'translate(0px, '+ wScroll/.4 +'%)'
+    });
 
+    // About section
+    $('.about .p-left').css({
+        'transform' : 'translateX(' + Math.max(wScroll / 1.5, 500) + 'px)'
+    });
 
+    $('.about .p-right').css({
+        'transform' : 'translateX(' + Math.min(-wScroll / 1.5, -500) + 'px)'
+    });
+}
 
+// Fungsi untuk reset efek parallax pada mobile
+function resetParallax() {
+    // Reset semua transformasi untuk menghindari efek parallax
+    $('.jumbotron-main h1, .jumbotron-main p, .jumbotron-main button').css('transform', 'none');
+    $('.about .p-left, .about .p-right').css('transform', 'none');
+}
+
+// Memanggil fungsi ketika halaman digulir
+$(window).scroll(function() {
+    checkMediaQuery();  // Mengecek apakah media query cocok dan menerapkan efek
 });
+
+// Juga memanggil fungsi saat pertama kali halaman dimuat untuk memeriksa kondisi media query
+$(document).ready(function() {
+    checkMediaQuery();
+});
+
+
+
 
 
 
